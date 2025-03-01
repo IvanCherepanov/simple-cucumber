4fabcaeace741bfe58e570eeebb2369101794fe8
Ivan <iv.cherepanov@yandex.ru>
2025-03-02 00:30:04 +0300

    task-1. add calculator app

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -17,12 +17,44 @@ class IsItFriday {
 }
 
 
+class Calculator {
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
+
+
 public class StepDefinitions {
     private String today;
     private String actualAnswer;
     private double cardBalance;
     private double productPrice;
     private String paymentResultMessage;
+
+    private int firstNumber;
+    private int secondNumber;
+    private int result;
+
+    @Дано("первое число равно {int}")
+    public void первое_число_равно(int number) {
+        this.firstNumber = number;
+    }
+
+    @Дано("второе число равно {int}")
+    public void второе_число_равно(int number) {
+        this.secondNumber = number;
+    }
+
+    @Когда("пользователь выполняет операцию сложения")
+    public void пользователь_выполняет_операцию_сложения() {
+        this.result = Calculator.add(firstNumber, secondNumber);
+    }
+
+    @Тогда("результат равен {int}")
+    public void результат_равен(int expectedResult) {
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
 
     @Given("today is Sunday")
     public void today_is_Sunday() {
